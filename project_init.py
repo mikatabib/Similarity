2fca141cc8a6e8b79c4f3a2ec387e3fbdfb4f199
mikatabib <mikatabib@protonmail.com>
2022-10-08 20:23:18 +0800

    create resource directory if it's doesn't exist

--- project_init.py
+++ project_init.py
@@ -4,12 +4,17 @@ import nltk.data
 from pathlib import Path
 
 CURRENT_DIR = Path(__file__).parent
-LOCAL_WN_DIRECTORY = str((CURRENT_DIR / "wn_data").absolute())
-LOCAL_NLTK_DIRECTORY = str((CURRENT_DIR / "tagger" / "nltk_data").absolute())
+LOCAL_WN_DIRECTORY = CURRENT_DIR / "wn_data"
+LOCAL_NLTK_DIRECTORY = CURRENT_DIR / "tagger" / "nltk_data"
 
-wn.config.data_directory = LOCAL_WN_DIRECTORY
+if not LOCAL_WN_DIRECTORY.exists():
+    LOCAL_WN_DIRECTORY.mkdir()
 
-nltk.download("punkt", LOCAL_NLTK_DIRECTORY)
+wn.config.data_directory = str(LOCAL_WN_DIRECTORY.absolute())
+wn.download("oewn:2021")
+wn.download("omw-id:1.4")
 
-# wn.download("oewn:2021")
-# wn.download("omw-id:1.4")
+if not LOCAL_NLTK_DIRECTORY.exists():
+    LOCAL_NLTK_DIRECTORY.mkdir()
+
+nltk.download("punkt", str(LOCAL_NLTK_DIRECTORY.absolute()))
